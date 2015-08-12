39812e46c7df6532ae218d39bcd045e0ce1b335c
Jane Zadorozhna <izadorozhna@mirantis.com>
2015-08-12 13:02:40 +0300

    Set path to swift func tests and tests are discovered
    Change-Id: Ic809f736bfa22fc27335df01ec37db28acc504b0

--- tempest/test_plugins/swift_plugin/swift_plugin.py
+++ tempest/test_plugins/swift_plugin/swift_plugin.py
@@ -2,9 +2,9 @@ from tempest.test_discover import plugins
 
 
 class SwiftPlugin(plugins.TempestPlugin):
-
     def load_tests(self):
-        return
+        return (".venv/src/swift/test/functional",
+                ".venv/src/swift/test/functional")
 
     def register_opts(self, conf):
         return
